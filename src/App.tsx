e91d3a16fc98ca21e2d9e9df0ac0603f458b9d92
Chris Coleman <chriscoleman@executivehomes.com>
2022-11-04 08:39:17 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,193 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <main className="">
+        <div className="left-container">
+          <div className="left-content-pad">
+            <h2>Scrollable left grid container</h2>
+            <ul>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+            </ul>
+          </div>
+        </div>
+        <div className="right-container">
+          <div className="right-content-pad">
+            <h1>Hello World</h1>
+            <ul>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+              <li>Lots of list items</li>
+            </ul>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
